7bc4dc223b247e3e37a16f3d85eac6fead3573ce
Nishant <0264nishu@gmail.com>
2024-11-24 21:17:23 +0530

    MARK IV

---
+++ js/functions.js
@@ -0,0 +1,21 @@
+//Camel Casing
+
+function hello() { //fucntion keyword use krna h
+    console.log("Hello, World!");
+    document.write("Hola Amigo!")
+}
+
+
+console.log(hello());
+
+//Functions with parameters
+var a = 10;
+var b = 20;
+
+function sum (x,y){
+    var ans = x + y;
+    document.write(ans+ "<br>");
+    console.log(ans);
+    
+}
+sum(10,59);
