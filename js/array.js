e7fe557d4dbdbc90ce3af3cbe0161ea8ae3c2a10
Nishant Garg <0264nishu@gmail.com>
2024-12-02 21:48:59 +0530

    MARK VI

---
+++ js/array.js
@@ -0,0 +1,97 @@
+//Methods to create arrays...
+const array = [1, 2, 3, 4, 5];
+const array2 = ["Nishant", 264, "Harsh"];
+const array3 = new Array(3);
+const array4 = new Array(1, "Nishant", "Garg", 9.56);
+array3[0] = 64;
+array3[1] = 32;
+array3[2] = "Nishant";
+console.log(array[1]);
+var sum = 0;
+
+for (var i = 0; i < 5; i++) {
+    console.log("<li>" + array[i] + "</li>");
+    sum += array[i];
+}
+
+console.log(sum);
+
+//Multi-Dimensional Array
+
+var array5 = [
+    [//0
+        1, 2, 3, 4, 5
+    ],
+    [//1
+        6, 7, 8, 9, 10
+    ],
+    [//2
+        11, 12, 13, 14, 15
+    ]
+]
+//do not use the "" inside the "" use ' in the document.write
+//we would use nested loof for traversing the multi-dimensional array
+
+//array.length -> to get the length of the array
+
+for(var a =0; a<array5.length; a++){
+    for(var b = 0; b<array5[a].length; b++){
+        console.log(array5[a][b]);
+    }
+}
+
+//Modifying the elements of array
+console.log(array[0]);
+array[0]= "Nishant";
+console.log(array[0]); //using the index method or directly using the index of the array
+
+//Removing the array Element
+delete array5[2][4];
+//It justs empty the space of the element but does not remove it from the array
+for(var a =0; a<array5.length; a++){
+    for(var b = 0; b<array5[a].length; b++){
+        console.log(array5[a][b]);
+    }
+}
+
+//Array Fucntion Methods
+
+const names = ["Nishant", "Harsh", "Ayush", "Yachna", "Das", "Abbas"];
+// console.log(names.indexOf("Harsh")); //returns the index of the element
+console.log(names);
+names.sort(); //sort the array lexographically or increasing order
+console.log(names);
+
+names.reverse();//reverses the array
+console.log(names);
+
+names.pop() //Removes the last element of the array
+console.log(names);
+
+names.push("Abbas"); //Add the element to the last of the array
+console.log(names);
+
+// names.shift(); //Removes the first element of the array
+// console.log(names);
+
+names.unshift("Yachna"); //Add the element to the first of the array
+console.log(names);
+
+//Concat the array with the another array
+
+//Method - 1
+var area = names.concat("West Delhi", "South Delhi", "Faridabad");
+// console.log(area);
+
+//Method - 2
+const college = ["GTBIT", "GTB4CEC"];
+var c = names.concat(area);
+// console.log(c);
+
+//Method - 3
+var c = [...names, ...area, ...college] //Spread Operator
+console.log(c);
+
+var d = names.join(" "); //Join the array with the string
+console.log(d);
+
