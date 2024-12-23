3d691a52ccd1e43914e542529e2772cb60e20fba
Nishant Garg <0264nishu@gamil.com>
2024-12-23 22:27:50 +0530

    Slice, Splice and isArray() method

--- js/array.js
+++ js/array.js
@@ -48,11 +48,11 @@ console.log(array[0]); //using the index method or directly using the index of t
 //Removing the array Element
 delete array5[2][4];
 //It justs empty the space of the element but does not remove it from the array
-for(var a =0; a<array5.length; a++){
-    for(var b = 0; b<array5[a].length; b++){
-        console.log(array5[a][b]);
-    }
-}
+// for(var a =0; a<array5.length; a++){
+    // for(var b = 0; b<array5[a].length; b++){
+        // console.log(array5[a][b]);
+    // }
+// }
 
 //Array Fucntion Methods
 
@@ -94,4 +94,43 @@ console.log(c);
 
 var d = names.join(" "); //Join the array with the string
 console.log(d);
+
+//Slice the array
+//slice(start, end) -> start is inclusive and end is exclusive
+var e = names.slice(1, 3);
+console.log(e);
+
+var e = names.slice(-3); //slice the last 3 elements
+console.log(e);
+
+var e = names.slice(2); //slice the elements from 2 to end
+// console.log(e);
+
+var e = names.slice(-3,-1);
+var e = names.slice() //slice the entire array
+
+//Splice the array--> It is used to add or remove the elements from the array
+//splice(start, howmany_to_delete, "new_value") 
+
+//..>start is the index from where to start deleting the elements  
+//..>howmany_to_delete is the number of elements to delete
+//..>new_value is the value to be added at the start index
+var numbers = [1, 2, 3, 4, 5, 6, 7];
+numbers.splice(2, 2, "Nishant", "Garg");
+console.log(numbers);
+
+numbers.splice(2, 0, "Nishant", "Garg");
+console.log(numbers);
+
+//it will be same as the slice in the case for the negative indexing or the one when it start from the end
+
+//isArray() function
+
+//if any element is an array then it will return true otherwise false
+
+console.log(Array.isArray(numbers));
+
+var a = 1;
+console.log(Array.isArray(a));
+
 
--- js/functions.js
+++ js/functions.js
@@ -1,11 +1,9 @@
 //Camel Casing
 
-function hello() { //fucntion keyword use krna h
+function hello() { //function keyword use krna h
     console.log("Hello, World!");
-    document.write("Hola Amigo!")
+    // document.write("Hola Amigo!")
 }
-
-
 console.log(hello());
 
 //Functions with parameters
@@ -14,20 +12,89 @@ var b = 20;
 
 function sum (x,y){
     var ans = x + y;
-    document.write(ans+ "<br>");
+    // document.write(ans+ "<br>");
     console.log(ans);
     
 }
 sum(10,59);
 
-//Functions with return type
+function name(first_name, last_name){
+    var full_name = first_name + " " + last_name;
+    console.log(full_name); 
+}
+name("ECE", "2027");
+//if one parameter is missing then it will be undefined (printed)
 
+
+//Functions with return type -- when the value is to be used somewhere else
 function add(a,b){
     //statements
     //return value
     return (a+b);
     // alert("Sayonara");
 }
+
+var return_value = add(4,5);
+console.log(return_value);
 // console.log(add(4,5));
 
 //Events in javascript..
+// 1. onclick   
+// 2. onmouseover
+// 3. onmouseout
+// 4. onkeydown
+// 5. onkeyup
+// 6. onkeypress
+// 7. onload
+// 8. onunload
+// 9. onscroll
+// 10. onresize
+// 11. onfocus
+// 12. onblur
+// 13. onchange
+// 14. onselect
+// 15. onsubmit
+// 16. onreset
+// 17. onmousedown
+// 18. onmouseup
+// 19. onmousemove
+// 20. onmouseout
+// 21. onmouseover
+// 22. ondblclick
+// 23. oncontextmenu
+// 24. ondragstart
+// 25. ondragend
+// 26. ondragenter
+// 27. ondragleave
+// 28. ondragover
+// 29. ondrop
+// 30. ondrag
+// 31. ondragend
+// 32. ondragenter
+// 33. ondragleave
+// 34. ondragover
+// 35. ondragstart
+// 36. ondrop
+
+//Event Listener
+// 1. addEventListener()
+// 2. removeEventListener()
+// 3. dispatchEvent()
+// 4. captureEvent()
+// 5. stopPropagation()
+// 6. preventDefault()
+// 7. stopImmediatePropagation()
+// 8. currentTarget
+// 9. target
+// 10. eventPhase
+// 11. bubbles
+// 12. cancelable
+// 13. defaultPrevented
+
+
+
+
+
+
+
+
