5252ddcdafd03384caccba7396497725cc0c7f77
Nishant Garg <0264nishu@gamil.com>
2024-12-24 11:34:49 +0530

    Math Methods

---
+++ js/mathMethods.js
@@ -0,0 +1,31 @@
+//Moslty used in animations, accounting websites 
+
+//Math is neccessary to use before methods
+
+var a = Math.ceil(2.1); //returns the greater integer of a number
+var a = Math.ceil(-2.65); //-2
+
+var a = Math.floor(2.7); //returns the smaller integer of a number
+var a = Math.floor(-2.65); //-3
+
+var a = Math.round(2.75165651); //rounds to the nearest integer -->3
+var a = Math.round(2.1646461651); //2
+
+var a = Math.trunc(8.65456); //just return the integer value removing the decimal palces
+
+var a = Math.max(10,22,3,99,88);
+
+var a = Math.min(-10, -2);
+
+var a = Math.sqrt(100); //returns the square root of a number
+var a = Math.cbrt(625);
+
+var a = Math.pow(10,5); //first is base and second is exponent
+
+var a = Math.random(); // by default it is between the 0 and 1.
+var a = Math.floor(Math.random() * 10) + 1; // returns a random number between 1 and 10
+
+var a = Math.abs(-10); //returns the absolute value of a number -->10
+
+console.log(a);
+console.log(Math.PI);
