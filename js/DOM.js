5823a6ab954f39bb15488a6c5b089228db16f49c
nishu-02 <0264nishu@gamil.com>
2024-12-29 20:09:07 +0530

    DOM Get & Set Methods

--- js/DOM.js
+++ js/DOM.js
@@ -37,3 +37,38 @@ element = document.getElementsByClassName("list")[0]; //returns the first elemen
 element = document.getElementsByTagName("ul"); 
 //just like the className we can use indexing
 element = document.getElementsByTagName("ul")[0]; //returns the first ul tag in the document
+
+
+//DOM GET METHODS:-
+
+element = document.getElementById("content").innerText; //returns the text content of the element with the id of content
+element = document.getElementById("content").innerHTML; //returns the html content of the element with the id of content
+
+//we can also use the getAttribute method to get the attribute of the element
+element = document.getElementById("content").getAttribute("class"); //returns the class attribute of the element with the id of content
+
+element = document.getElementById("content").getAttributeNode("class"); //return the property with name and value
+element = document.getElementById("conten").getAttributeNode("class").value; // return the value just like the getAttribute() so no use of this method
+//we can also use the getAttributeNames() method to get the attribute names of the element
+element = document.getElementById("content").getAttributeNames(); //returns the array of attribute names of the element
+
+element = document.getElementById("header").attributes; //returns the array of attributes of the element with the id of header
+element = document.getElementById("header").attributes[3];
+element = document.getElementById("header").attributes[2].name;
+
+
+//DOM SET METHODs:-
+
+element = document.getElementById("content").innerText = "Hello World"; //sets the text content of the element with the id of content
+//this method cannot use html so we use innerHTML;
+
+element = document.getElementById("content").innerHTML = "<h2>Nishu-02</h2>"
+
+//if we wish to set the attribute of it 
+
+element = document.getElementById("header").setAttribute("class", "header-class");
+// first parameter is the class name and second is the value of the class value
+
+element = document.getElementById("header").attributes[2].value = "xyz";
+
+element = document.getElementById("header").removeAttribute("name");
